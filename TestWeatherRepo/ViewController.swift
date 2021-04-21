11de506a8bbacd60dd7015f9a504c6c120a3fae6
Andi Radita <inyoman.andiradita@gmail.com>
2021-04-22 00:39:10 +0800

    adding action button pop up an alert

--- TestWeatherRepo/ViewController.swift
+++ TestWeatherRepo/ViewController.swift
@@ -16,6 +16,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func actionButton(_ sender: UIButton) {
+        print("hello guys welcome back")
+        let alert = UIAlertController(title: "Did you bring your towel?", message: "It's recommended you bring your towel before continuing.", preferredStyle: .alert)
 
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true)
+    }
+    
 }
 
