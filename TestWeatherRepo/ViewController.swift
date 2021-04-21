6cde669dc2f1832a0197310674e2c1391cc9ca31
Andi Radita <inyoman.andiradita@gmail.com>
2021-04-22 00:28:12 +0800

    create layout and linked to view controller

--- TestWeatherRepo/ViewController.swift
+++ TestWeatherRepo/ViewController.swift
@@ -9,8 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LabelText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.LabelText.text = "Hello Broww"
         // Do any additional setup after loading the view.
     }
 
